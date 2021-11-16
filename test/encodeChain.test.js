4148e545829bb99372a8deb7c3a4dbe74db3f5d2
Pavel Sashnikov <psashnikau@datamola.com>
2021-11-16 21:41:00 +0300

    feat: cover chain

---
+++ test/encodeChain.test.js
@@ -0,0 +1,23 @@
+const { encodeChain } = require("../src/encodeChain");
+const { TransformEncodeA } = require("../src/stream/transformA");
+const { TransformEncodeC } = require("../src/stream/transformC");
+const { TransformEncodeR } = require("../src/stream/transformR");
+
+describe("Encode chain", () => {
+  test("should be filled arr", () => {
+    expect(encodeChain("A")).toHaveLength(1);
+    expect(encodeChain("C1")).toHaveLength(1);
+    expect(encodeChain("R0-R1")).toHaveLength(2);
+    expect(encodeChain("R0-A-C1")).toHaveLength(3);
+  });
+
+  test("should be empty arr", () => {
+    expect(encodeChain("")).toHaveLength(0);
+  });
+
+  test("should be instance", () => {
+    expect(encodeChain("A")[0]).toBeInstanceOf(TransformEncodeA);
+    expect(encodeChain("C1")[0]).toBeInstanceOf(TransformEncodeC);
+    expect(encodeChain("R0")[0]).toBeInstanceOf(TransformEncodeR);
+  });
+});
